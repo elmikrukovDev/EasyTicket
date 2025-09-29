1922fde3eecadc9dbacdcf8c934b6892f16dc5db
Daniil Mikrukov <elmikrukov@outlook.com>
2025-09-30 00:31:20 +0500

    Добавлены DbSet в контекст

--- backend/src/AuthService/Infrastructure/AuthService.Infrastructure.EntityFramework/Contexts/AuthDbContext.cs
+++ backend/src/AuthService/Infrastructure/AuthService.Infrastructure.EntityFramework/Contexts/AuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using AuthService.Infrastructure.EntityFramework.Configurations;
+﻿using AuthService.Domain.Entities;
+using AuthService.Infrastructure.EntityFramework.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuthService.Infrastructure.EntityFramework.Contexts;
@@ -10,6 +11,10 @@ public class AuthDbContext : DbContext
     {
         
     }
+
+    public DbSet<Role> Roles { get; set; }
+
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
