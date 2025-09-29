9e9c19518679e9129501a2ae1b2af083992cc7fa
Daniil Mikrukov <elmikrukov@outlook.com>
2025-09-29 22:12:08 +0500

    Добавлены связи и конфигурация модели базы данных, также реализован контекст подключения к базе данных

---
+++ backend/src/AuthService/Infrastructure/AuthService.Infrastructure.EntityFramework/Configurations/UserConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using AuthService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuthService.Infrastructure.EntityFramework.Configurations;
+
+internal class UserConfiguration
+    : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(u => u.UserName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(u => u.DisplayName)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder.Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(256);
+
+        builder.Property(u => u.PhoneNumber)
+            .IsRequired(false)
+            .HasMaxLength(25);
+
+        builder.HasOne(u => u.Role)
+            .WithMany(r => r.Users)
+            .HasForeignKey(u => u.RoleId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(u => u.UserName).IsUnique();
+        builder.HasIndex(u => u.Email).IsUnique();
+        builder.HasIndex(u => u.DisplayName).IsUnique();
+        builder.HasIndex(u => u.PhoneNumber).IsUnique();
+    }
+}
